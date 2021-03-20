c80728614d1997235cd5a8c2b30d47264a02c55a
Alfonso Lasa <alfonsolasaguirre@gmail.com>
2021-03-20 22:31:24 +0100

    add new colors

--- src/Colors.ts
+++ src/Colors.ts
@@ -3,5 +3,7 @@ export default {
     yellow: 'rgb(224, 255, 51)',
     green: 'rgb(119, 255, 51)',
     blue: 'rgb(51, 54, 255)',
-    pink: 'rgb(233, 51, 255)'
+    pink: 'rgb(233, 51, 255)',
+    black: 'rgb(33, 47, 60)',
+    white: 'rgb(247, 249, 249)'
 }
